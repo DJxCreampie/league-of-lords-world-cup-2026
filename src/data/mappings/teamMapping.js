ab0dd8f0fe0c5e027067c1873922f110c2cb4dc0
DJxCreampie <81438379+DJxCreampie@users.noreply.github.com>
2026-05-06 21:51:17 -0400

    Add isolated football-data.org field-discovery spike

---
+++ src/data/mappings/teamMapping.js
@@ -0,0 +1,19 @@
+export const TEAM_ID_BY_API_ID = {
+  'api-usa': 'team-united-states',
+  'api-bra': 'team-brazil',
+  'api-eng': 'team-england',
+  'api-fra': 'team-france',
+  'api-arg': 'team-argentina',
+  'api-jpn': 'team-japan',
+}
+
+export const TEAM_ID_BY_API_NAME = {
+  'united states': 'team-united-states',
+  usa: 'team-united-states',
+  usmnt: 'team-united-states',
+  brazil: 'team-brazil',
+  england: 'team-england',
+  france: 'team-france',
+  argentina: 'team-argentina',
+  japan: 'team-japan',
+}
